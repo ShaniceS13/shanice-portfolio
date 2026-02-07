1ee8d00c0b72f299bb233bf67216cbd286bfb7dd
ShaniceS13 <shanicesolomon2@gmail.com>
2026-02-07 14:38:39 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Sidebar from "./Sidebar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+
+      <main className="main">Main content</main>
+    </div>
+  );
+}
